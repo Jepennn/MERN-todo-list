dd5df1b78e6b7f145c4c60cecbe45ea5a45e5b27
Jesper Hesselgren <jeppehesselgren@gmail.com>
2024-09-25 23:10:43 +0200

    Day 4, completed get, post, put routes for todos. Changed name on todos controller class.

---
+++ Backend/controller/todos.controller.js
@@ -0,0 +1,63 @@
+import mongoose from "mongoose";
+import { Todo } from "../model/todos.model.js";
+
+// In the controller, we will define the functions that will be used in the routes.
+// The controller will interact with the database using the model.
+// The controller will also send responses to the client/view.
+
+// Get all todos
+const getTodos = async (req, res) => {
+  try {
+    const todos = await Todo.find({}); //find all the todos in the database
+    res.json(todos);
+  } catch (error) {
+    res.status(500).json({ succes: false, message: error.message });
+  }
+};
+
+//Create a new todo
+const createTodo = async (req, res) => {
+  const todo = req.body; // Data that user sends in the request
+
+  if (!todo.body || !todo.status) {
+    return res
+      .status(400)
+      .json({ message: "Please fill in all the required fields" });
+  }
+  const newTodo = new Todo(todo); //create a new todo
+
+  try {
+    await newTodo.save(); //save the todo to the database
+    res.status(201).json({ succes: true, data: newTodo }); //send the saved todo as a response
+  } catch (error) {
+    console.log("erro in creating product", error);
+    res.status(500).json({ succes: false, message: error.message });
+  }
+};
+
+// Update a todo
+const updateTodo = async (req, res) => {
+  const { id } = req.params; // takes the parameters in the url
+  const updates = req.body; // Data that user sends in the request
+
+  try {
+    const updatedTodo = await Todo.findByIdAndUpdate(id, updates, {
+      new: true,
+      runValidators: true,
+    }); //find the todo by id and update it and return the updated todo, runValidators will check the schema for validation
+    res.status(200).json({ succes: true, data: updatedTodo });
+  } catch (error) {
+    res.status(500).json({ succes: false, message: error.message });
+  }
+};
+
+// Delete a todo
+const deleteTodo = async (req, res) => {
+  try {
+    res.json({ message: "Deleted a todo" });
+  } catch {
+    res.status(500).json({ succes: false, message: error.message });
+  }
+};
+
+export { getTodos, createTodo, updateTodo, deleteTodo };
--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,12 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import { connectDB } from "./db.js";
 import { Todo } from "./model/todos.model.js";
+import {
+  getTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+} from "./controller/todos.controller.js";
 
 dotenv.config(); //load env variables
 
@@ -14,28 +20,23 @@ connectDB()
   .then(() => {
     //starts the server
     app.listen(port, () => {
-      console.log(`Server is running on port ${port}`);
+      console.log(`Server is running on port http://localhost:${port}`);
     });
   })
   .catch((error) => {
     console.error(`Failed to connect to the database: ${error.message}`);
   });
 
-// Todo routes starts here
+/*Todo routes starts here*/
 app.get("/", (req, res) => {
   res.redirect("/todos");
 });
 
-app.get("/todos", async (req, res) => {
-  try {
-    const todos = await Todo.find(); //returns all the todos
-  } catch (error) {
-    res.status(500).json({ succes: false, message: error.message });
-  }
-});
+//Done
+app.get("/todos", getTodos);
+//Done
+app.post("/todos", createTodo);
+//Done
+app.put("/todos/:id", updateTodo);
 
-app.post("/todos", (req, res) => {
-  try {
-  } catch {}
-  res.json({ message: "Created a todo" });
-});
+app.delete("/todos/:id", deleteTodo);
