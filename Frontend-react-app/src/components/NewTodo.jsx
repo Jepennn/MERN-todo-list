1eaa26d0a02fd8ddac91790d63e9332fde3d07be
Jesper Sandberg Hesselgren <jespersh@kth.se>
2024-10-23 23:15:17 +0200

    Fixed style for new todo input

--- Frontend-react-app/src/components/NewTodo.jsx
+++ Frontend-react-app/src/components/NewTodo.jsx
@@ -1,11 +1,31 @@
 import styles from "./NewTodo.module.css";
+import { useState } from "react";
 
 function NewTodo() {
+  const [todo, setTodo] = useState("Hej världen");
+
+  // const handleChange = (e) => {
+  //   setTodo(e.target.value);
+  // };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(todo);
+    setTodo(" ");
+  };
+
   return (
     <div className={styles.container}>
-      <form className={styles.form}>
-        <label htmlFor="todoInfo">Add Todo</label>
-        <textarea id="todoInfo" />
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <input
+          type="text"
+          id="todoInfo"
+          placeholder="Add your todo"
+          className={styles.inputForm}
+        />
+        <button onClick={handleSubmit} className={styles.buttonForm}>
+          Add
+        </button>
       </form>
     </div>
   );
